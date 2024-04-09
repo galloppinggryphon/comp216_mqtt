48fb5a8146ea97426c2cf08e8c76aa96f0f37455
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2024-04-09 11:45:11 -0400

    Added simulator launcher

---
+++ app/_simulator_test.py
@@ -0,0 +1,13 @@
+from app.api.iot_simulator import IoTSimulator
+from app.gui.main_window import MainWindow
+from app.config import device_config, mqtt_config
+from app.main import Main
+
+#Configure logger
+Main.configure_logger()
+
+dev1 = device_config[0]
+
+IoTSimulator(mqtt_config)
+IoTSimulator.create_publisher(dev1)
+IoTSimulator.start_publisher(dev1.id)
--- app/main.py
+++ app/main.py
@@ -1,15 +1,45 @@
-from app.api.data_generator import DataGenerator
-from app.api.mqtt_controller import MQTTController
-from app.gui.clients.client_window_1 import ClientWindow1
+import sys
+import threading
+import logging
+
+from app.api.iot_simulator import IoTSimulator
 from app.gui.main_window import MainWindow
-from app.config import mqtt_config
+from app.config import device_config, mqtt_config
+
 
 class Main:
     def __init__(self):
-        MQTTController(mqtt_config)
+        self.configure_logger()
+        logging.info('Starting app')
+        self.start_simulator_thread()
+
         app = MainWindow()
-        # app = ClientWindow1()
         app.mainloop()
+
+    @staticmethod
+    def configure_logger():
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt="[%H:%M:%S]")
+
+        # Continuous output to screen
+        log_handler = logging.StreamHandler(sys.stdout)
+        log_handler.setFormatter(formatter)
+        logging.getLogger().addHandler(log_handler)
+        logging.getLogger().setLevel(logging.DEBUG)
+
+
+    #TODO: Not sure if the whole simulator should be threaded or just MQTT clients when they're started
+    def start_simulator_thread(self):
+        logging.info('Starting simulator thread')
+        sim_thread = threading.Thread(target=self.start_simulator, daemon=True) # Background thread
+        sim_thread.start()
+
+    def start_simulator(self):
+        logging.debug('Hello from the simulator thread')
+
+        IoTSimulator(mqtt_config)
+        IoTSimulator.create_publisher(device_config[0])
+        IoTSimulator.start_publisher(device_config[0].id)
+
 
 
 if __name__ == '__main__':
