65b7c2f6ec5a11111992dd5c38658f7b772947f5
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2024-04-09 15:13:19 -0400

    Improved GUI, stop demo

--- app/gui/main_window.py
+++ app/gui/main_window.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+import logging
+import tkinter as tk
+
 ### IMPORT ALL COMPONENTS FROM TTK IF POSSIBLE! ###
 from tkinter.ttk import Frame, Button, Label
 
@@ -6,38 +10,71 @@ from app.config import theme_config, window_configs
 from app.gui.clients.client_window_1 import ClientWindow1
 from app.gui.framework.tkwindow import TKWindow
 
-
 class MainWindow(TKWindow):
-    def __init__(self):
-        super().__init__(True, window_configs.main_window_config, theme_config.ThemeConfig, theme_config.window_styles)
+    device_list_container: Frame
+    client_list_container: Frame
 
+    def __init__(self):
+        super().__init__(True, window_configs.main_window_config,
+                         theme_config.ThemeConfig, theme_config.window_styles)
         self.main_section()
 
-
     def main_section(self):
-        main = self.main
-        wrapper = Frame(main)
-        # wrapper.grid(row=0, column=2)
-        client_box = Frame(wrapper)
-        sensor_box = Frame(wrapper)
+        wrapper = Frame(self.main)
+        wrapper.grid_columnconfigure(0, weight=1)
+        wrapper.grid_columnconfigure(1, weight=1)
+        wrapper.grid_rowconfigure(0)
+        wrapper.pack(expand=True, fill=tk.BOTH)
 
-        wrapper.pack()
-        sensor_box.pack(pady=(10, 10))
-        client_box.pack()
+        self.device_list_container = Frame(wrapper)
+        # device_list_box.pack(pady=(0, 25))
+        self.device_list_container.grid(row=0, column=0)
 
-        padding = 2
+        self.client_list_container = Frame(wrapper)
+        self.client_list_container.grid(row=0, column=1)
 
-        Label(sensor_box, text="Sensors",justify="left").pack(padx=(padding, padding), pady=(padding, padding))
-        Button(sensor_box, text="Start Sensor 1", command=self.start_sensor_1).pack(padx=(padding, padding), pady=(padding, padding))
+        self.draw_client_list()
+        self.draw_device_list()
 
-        Label(client_box, text="Clients",justify="left").pack(padx=(padding, padding), pady=(padding, padding))
-        Button(client_box, text="Open Client 1", command=self.open_client_1).pack(padx=(padding, padding), pady=(padding, padding))
-        Button(client_box, text="Open Client 2", command=self.open_client_1).pack(padx=(padding, padding), pady=(padding, padding))
+    def draw_device_list(self):
+        spacing_y = 2
+        spacing_x = 5
+
+        parent = self.device_list_container
+
+        # Devices
+        Label(parent, text="IoT devices", justify="left").pack(
+            padx=(spacing_x, spacing_x), pady=(0, 5))
+
+        row1 = Frame(parent)
+        row1.pack(pady=(spacing_y, spacing_y), side=tk.BOTTOM)
+        Label(row1, text="Device 1", justify="left").pack(
+            padx=(spacing_x, spacing_x))
+        Button(row1, text="Start", command=self.start_device_1).pack(
+            padx=(spacing_x, spacing_x), side=tk.LEFT)
+        Button(row1, text="Stop", command=self.stop_device_1).pack(
+            padx=(spacing_x, spacing_x), side=tk.LEFT)
+
+    def draw_client_list(self):
+        spacing_y = 2
+        spacing_x = 5
+
+        parent = self.client_list_container
+
+        Label(parent, text="Clients", justify="left").pack(
+            padx=(spacing_x, spacing_x), pady=(0, 5))
+        Button(parent, text="Open Client 1", command=self.open_client_1).pack(
+            padx=(spacing_x, spacing_x), pady=(spacing_y, spacing_y))
+        Button(parent, text="Open Client 2", command=self.open_client_1).pack(
+            padx=(spacing_x, spacing_x), pady=(spacing_y, spacing_y))
+
 
     def open_client_1(self):
         ClientWindow1()
 
-    def start_sensor_1(self):
 
-        #TODO: feedback on screen if connection fails
+    def start_device_1(self):
         IoTSimulator.start_publisher('temp_sensor')
+
+    def stop_device_1(self):
+        IoTSimulator.stop_publisher('temp_sensor')
