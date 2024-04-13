43ad399f67d7c54da68145d4e083ab49c90005c7
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2024-04-13 17:26:41 -0400

    Rm unused

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,6 +1,6 @@
 # This file exports all the modules in this package (folder)
 # Imports are automatically re-exported
-from app.config import theme_config, main_config, window_configs, mqtt_config as _mqtt_config
+from app.config import theme_config, window_configs, mqtt_config as _mqtt_config
 
 mqtt_config = _mqtt_config.connection_settings
 device_config = _mqtt_config.device_config
