dc2c647b8ebbce0a198fe08e0b1bf954f790a67f
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2024-04-15 15:52:53 -0400

    Init publishers

--- app/main.py
+++ app/main.py
@@ -5,15 +5,22 @@ from app.api.iot_simulator import IoTSimulator
 from app.gui.main_window import MainWindow
 from app.config import device_config, mqtt_config
 
-
 class Main:
     def __init__(self):
         self.configure_logger()
         logging.info('Starting app')
-        logging.info('Starting simulator')
-        IoTSimulator(mqtt_config)
+
+        self.start_simulator()
+
         app = MainWindow()
         app.mainloop()
+
+    def start_simulator(self):
+        logging.info('Starting simulator')
+        IoTSimulator(mqtt_config)
+
+        for device in device_config:
+            IoTSimulator.create_publisher(device)
 
     @staticmethod
     def configure_logger():
@@ -24,7 +31,6 @@ class Main:
         log_handler.setFormatter(formatter)
         logging.getLogger().addHandler(log_handler)
         logging.getLogger().setLevel(logging.DEBUG)
-
 
 
 if __name__ == '__main__':
